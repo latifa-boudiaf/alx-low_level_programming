9c0bb2bfc1f5b2768ccc27a47affb5f736a9c2ca
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-06 16:29:14 -0700

    third commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,26 +9,23 @@
  */
 int main(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i <= 8; i++)
-    {
-        for (j = i + 1; j <= 9; j++)
-        {
-            /* Print the two digits in ascending order */
-            putchar('0' + i);
-            putchar('0' + j);
-
-            /* Print a comma and space if not the last combination */
-            if (i != 8 || j != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-
-    putchar('\n');
-
-    return (0);
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			/* Print the two digits in ascending order */
+			putchar('0' + i);
+			putchar('0' + j);
+			/* Print a comma and space if not the last combination */
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
