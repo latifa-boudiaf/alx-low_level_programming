81bac7b5f30b29aab75908c5a6da70731f18e93d
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-06 12:16:10 -0700

    first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-/**
+/*
  * The main function of the program.
  * It prints a message indicating whether a number is positive or negative.
  */
@@ -14,9 +14,9 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if( n > 0 )
+	if (n > 0)
 		printf("%d is positive\n", n);
-	else if( n == 0 )
+	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
