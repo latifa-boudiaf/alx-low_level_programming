7cd2f74aad25e7bd2af917b5bdca1063850d207d
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-06 15:22:34 -0700

    third commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* prints all letters in lowercase except e and q */
+/**
+ * main- entry point of the program.
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
