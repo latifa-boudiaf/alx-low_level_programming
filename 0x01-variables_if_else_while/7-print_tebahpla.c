e075fe7928297cf3cd4f9a8f771729643f45de9b
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-06 16:36:53 -0700

    third commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the lowercase alphabet in reverse, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
