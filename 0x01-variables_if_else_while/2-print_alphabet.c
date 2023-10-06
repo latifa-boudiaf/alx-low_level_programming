88cd39a7bb94075a7d17b7df0157cf80a0a050a7
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-06 14:15:12 -0700

    third commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* This program prints the letters of the alphabet from a through z */
+
+/**
+ * main- the entry point of the C program.
+ * Return: 0(success)
+ */
+int main(void) {
+    char letter = 'a';
+
+    while (letter <= 'z') {
+        putchar(letter);
+        letter++;
+    }
+
+    putchar('\n');
+    return 0;
+}
