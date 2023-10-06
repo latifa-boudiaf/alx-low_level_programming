4e2da879187dde98b2eb6da6d2e9447fd9ae1e54
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-06 16:30:32 -0700

    third commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 0; i <= 9; i++)
+    {
+        putchar('0' + i);
+
+        /* Print a comma and space if not the last number */
+        if (i != 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
+
+    putchar('\n');
+
+    return (0);
+}
