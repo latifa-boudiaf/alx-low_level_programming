8efbb61b31e616be4fc7c90adc7ede135aacd099
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-06 16:32:29 -0700

    third commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,21 +9,18 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i <= 9; i++)
-    {
-        putchar('0' + i);
-
-        /* Print a comma and space if not the last number */
-        if (i != 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-
-    putchar('\n');
-
-    return (0);
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+		/* Print a comma and space if not the last number */
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
