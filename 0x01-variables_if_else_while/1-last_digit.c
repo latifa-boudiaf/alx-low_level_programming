2beaf821ee1d1cd054387902fa82670c723eb593
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-06 16:50:10 -0700

    third commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,34 +4,32 @@
 
 /**
  * main - Entry point
- *
- * Description: Assigns a random number to 'n' and prints the last digit of 'n'
- *
- * Return: Always 0 (Success)
+ * Assigns a random number to 'n' and prints the last digit of 'n'
+ * Return: 0(Success)
  */
 int main(void)
 {
-	int n;
+        int n, lastDigit;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
 
-	printf("Last digit of %d is ", n);
+        printf("Last digit of %d is ", n);
 
-	int lastDigit = n % 10;
+        lastDigit = n % 10;
 
-	if (lastDigit > 5)
-	{
-		printf("%d and is greater than 5\n", lastDigit);
-	}
-	else if (lastDigit == 0)
-	{
-		printf("%d and is 0\n", lastDigit);
-	}
-	else
-	{
-		printf("%d and is less than 6 and not 0\n", lastDigit);
-	}
+        if (lastDigit > 5)
+        {
+                printf("%d and is greater than 5\n", lastDigit);
+        }
+        else if (lastDigit == 0)
+        {
+                printf("%d and is 0\n", lastDigit);
+        }
+        else
+        {
+                printf("%d and is less than 6 and not 0\n", lastDigit);
+        }
 
-	return (0);
+        return (0);
 }
