1de38991b5f43855de8fdcc142265b4e1468650f
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-06 15:09:50 -0700

    third commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* prints all single digit numbers of base 10 */
+/**
+ * main- the entry point of the program.
+ * Return: 0(success).
+ */
+int main(void)
+{
+	int num;
+
+	for(num = 0; num < 10; num++)
+	{
+		printf("%d\n", num);
+	}
+	return (0);
+}
