e87370d339f8f541083841cc8d0d13d1703d689b
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-07 05:16:06 -0700

    whatever

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	char *message = "\"Programming is like building a multilingual puzzle";
+	puts(message);
+	return (0);
+}
