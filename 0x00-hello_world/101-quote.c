3a3a8f5adb4f1dd6ed6648f645ab965498cd888e
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-07 05:50:00 -0700

    final commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,10 @@
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - "
-	       "Dora Korpar, 2015-10-19\n";
+	char *message_part1 = "and that piece of art is useful\" - ";
+	char *message_part2 = "Dora Korpar, 2015-10-19\n";
 
-	write(STDERR_FILENO, message, 59);
+	write(STDERR_FILENO, message_part1, strlen(message_part1));
+	write(STDERR_FILENO, message_part2, strlen(message_part2));
 	return (1);
 }
