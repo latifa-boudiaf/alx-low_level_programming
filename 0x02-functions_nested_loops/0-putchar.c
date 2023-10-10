2f58dbfba85c70ee6aa6da1c8b63d3cfe38b35b7
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-10 10:18:46 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* a program that prints _putchar */
+/**
+ * main- entry point of the program.
+ * Return: 0(success).
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
