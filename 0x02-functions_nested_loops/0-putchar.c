27fe9df8016341f3b7b180f52e613808d3e617c8
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-10 13:00:17 -0700

    c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,8 +5,20 @@
  * main- entry point of the program.
  * Return: 0(success).
  */
-int _putchar(void)
+int main(void)
 {
-	write("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
+}
+int _putchar(char c)
+{
+	return write(1, &c, 1);
 }
