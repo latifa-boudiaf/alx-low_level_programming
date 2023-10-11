51d149dd728d3c808ece2c07674508a28ce328ba
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-11 12:57:44 -0700

    first

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,25 @@
 #include "main.h"
 
+/**
+ * print_alphaber_x10- prints the alphabets 10 times.
+ * Return: 0(success).
+ */
+
 void print_alphabet_x10(void)
 {
-    char letter = 'a';
-    int count = 0;
-
-    while (count < 10)
-    {
-        while (letter <= 'z')
-        {
-            _putchar(letter);
-            letter++;
-        }
-        letter = 'a'; /* Reset the letter to 'a' for the next iteration */
-        _putchar('\n');
-        count++;
-    }
+	char letter = 'a';
+	int count = 0;
+	
+	while (count < 10)
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		letter = 'a'; /* Reset the letter to 'a' for the next iteration */
+		_putchar('\n');
+		count++;
+	}
+	return (0);
 }
