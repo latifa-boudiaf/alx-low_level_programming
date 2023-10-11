e9b3da9183cc9720a1df30d0b2e97cbfa610f478
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-11 14:01:02 -0700

    first

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 	int limit = 1024; /* The limit (exclusive) */
-	int sum = 0;
+	int sum,i = 0;
 
-	for (int i = 1; i < limit; i++)
+	for(i = 1; i < limit; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
