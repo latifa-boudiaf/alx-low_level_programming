5731e99e78dc7fd37c66a854771b0d5f7f54bc34
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-11 13:04:56 -0700

    first

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower- checks if a character is lower case or not.
+ * Return: 0(success).
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return 1; /* c is a lowercase character */
+	}
+	else
+	{
+		return 0; /* c is not a lowercase character */
+	}
+}
