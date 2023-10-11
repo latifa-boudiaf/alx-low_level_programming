61f79324e8bc35e14c4dc574f04fe94f05c67e7f
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-11 13:36:00 -0700

    first

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Computes the sum of two integers
+ * @n: The first integer
+ * @m: The second integer
+ *
+ * Return: The absolute value of n.
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
