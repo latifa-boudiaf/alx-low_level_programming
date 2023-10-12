a8dd71bd2d14454d2c864e83e3196eb9eb2ad500
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-12 15:01:15 -0700

    third commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers 0 to 9 followed by a new line
+ */
+
+void print_numbers(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		_putchar(number + '0');
+		number++;
+	}
+
+	_putchar('\n');
+}
