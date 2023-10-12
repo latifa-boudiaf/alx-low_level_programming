5ff07665e70549672767849162156b2d98ed9812
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-12 16:38:53 -0700

    m

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,6 +4,7 @@
  * print_square - Prints a square of a specified size
  * @size: The size of the square
  */
+
 void print_square(int size)
 {
     if (size <= 0) {
@@ -13,10 +14,10 @@ void print_square(int size)
 
     int i, j;
 
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size; j++) {
-            _putchar('#'); 
-        }
-        _putchar('\n');
+    for (i = 0; i < size; i++)
+    {
+	    for (j = 0; j < size; j++)
+		    _putchar('#');
+	    _putchar('\n');
     }
 }
