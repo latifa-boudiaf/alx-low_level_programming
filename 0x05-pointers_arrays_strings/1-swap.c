7077b3600514f31895df939185db82669bc0f303
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-16 02:46:47 -0700

    second commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -7,8 +7,10 @@
  */
 void swap_int(int *a, int *b)
 {
-    int temp = *a; // Store the value of a in a temporary variable
-    *a = *b;       // Assign the value of b to a
-    *b = temp;     // Assign the original value of a (stored in temp) to b
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
