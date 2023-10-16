1640660f49c4cf2e02a554d00d2be56a364710e6
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-16 02:58:26 -0700

    forth commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _puts - Prints a string followed by a new line
- * @str: The string to print
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
  */
 void _puts(char *str)
 {
-    while (*str)
-    {
-        _putchar(*str);
-        str++;
-    }
-    _putchar('\n');
-}
+	int i;
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
