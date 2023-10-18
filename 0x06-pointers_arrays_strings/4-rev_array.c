ab2a48c3bc2ac0d27098b754db5111175fd469d0
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-18 13:01:19 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,25 +1,20 @@
 #include "main.h"
 
 /**
- * reverse_array - Reverses an array of integers
- * @a: The array of integers to reverse
- * @n: The number of elements in the array
+ * reverse_array - reverse the contents of an array of integers
+ * @a: array of integers
+ * @n: number of elements in array
  */
+
 void reverse_array(int *a, int n)
 {
-    int start = 0;
-    int end = n - 1;
-    int temp;
+	int i;
+	int h;
 
-    while (start < end)
-    {
-        /* Swap elements a[start] and a[end] */
-        temp = a[start];
-        a[start] = a[end];
-        a[end] = temp;
-
-        start++;
-        end--;
-    }
+	for (i = 0; i < (n / 2); i++)
+	{
+		h = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = h;
+	}
 }
-
