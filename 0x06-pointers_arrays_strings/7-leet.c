dd6ec424a2a513bb79a6b6db7ba165a78c94b65c
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-18 13:34:08 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,23 +5,24 @@
  * @p: the string to encode.
  * @i: iterates over the characters to encode.
  */
+
 char *leet(char *str)
 {
-        char *p = str;
-        int i;
+	char *p = str;
+	int i;
 
-        while (*str)
-        {
-                for (i = 0; i < 10; i++)
-                {
-                        if (*str == "aAeEoOtTlL"[i])
-                        {
-                                *str = "4433007711"[i];
-                                break;
-                        }
-                }
-        str++;
-        }
-        
-        return (p);
+	while (*str)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			if (*str == "aAeEoOtTlL"[i])
+			{
+				*str = "4433007711"[i];
+				break;
+			}
+		}
+		str++;
+	}
+
+	return (p);
 }
