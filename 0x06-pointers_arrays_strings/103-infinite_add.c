9682af1783264075a9eb42819eee6f368263aacb
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-20 13:00:16 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,49 +10,48 @@
  *
  * Return: A pointer to the result (in the buffer r) or 0 if it cannot fit.
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1, len2, carry, sum, index, i, j;
+    int len1, len2, carry, sum, index, i, j;
 
-	len1 = strlen(n1) - 1;
-	len2 = strlen(n2) - 1;
-	carry = 0;
-	index = 0;
+    len1 = strlen(n1) - 1;
+    len2 = strlen(n2) - 1;
+    carry = 0;
+    index = 0;
 
-	while (len1 >= 0 || len2 >= 0 || carry > 0)
-	{
-		sum = carry;
+    while (len1 >= 0 || len2 >= 0 || carry > 0)
+    {
+        sum = carry;
 
-		if (len1 >= 0)
-		{
-			sum += n1[len1] - '0';
-			len1--;
-		}
+        if (len1 >= 0)
+        {
+            sum += n1[len1] - '0';
+            len1--;
+        }
 
-		if (len2 >= 0)
-		{
-			sum += n2[len2] - '0';
-			len2--;
-		}
+        if (len2 >= 0)
+        {
+            sum += n2[len2] - '0';
+            len2--;
+        }
 
-		carry = sum / 10;
-		if (index >= size_r)
-			return 0;
+        carry = sum / 10;
+        if (index >= size_r - 1)
+            return 0;
 
-		r[index] = (sum % 10) + '0';
-		index++;
-	}
+        r[index] = (sum % 10) + '0';
+        index++;
+    }
 
-	r[index] = '\0';
+    r[index] = '\0';
 
-	for (i = 0, j = index - 1; i < j; i++, j--)
-	{
-		char temp = r[i];
-		r[i] = r[j];
-		r[j] = temp;
-	}
+    for (i = 0, j = index - 1; i < j; i++, j--)
+    {
+        char temp = r[i];
+        r[i] = r[j];
+        r[j] = temp;
+    }
 
-	return (r);
+    return (r);
 }
 
