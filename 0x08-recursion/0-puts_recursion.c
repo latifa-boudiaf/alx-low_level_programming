429803313e2a39494bdcfd827208d6d97db776c0
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-25 14:34:22 -0700

    commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * _puts_recursion - prints a string followed by a new line
+ * _puts_recursion- prints a string followed by a new line
  *
  * @s: The string to be printed 
  */
