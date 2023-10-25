df11bc113d7de36b8dce07a63018ed05aaaab819
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-25 14:47:40 -0700

    commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of a number
+ * @n: The number for which to calculate the factorial
+ * Return: The factorial of n, or -1 if n is less than 0
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+	else
+		return (n * factorial(n - 1));
+}
