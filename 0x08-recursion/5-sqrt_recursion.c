5383d8e38d347beb45ff209ff94b7543269ca7d8
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-25 15:24:14 -0700

    commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,19 +11,5 @@ int _sqrt_recursion(int n)
 	else if (n == 0 || n == 1)
 		return (n);
 	else
-		sqrt_helper(n, 1);
-}
-/**
- * sqrt_helper - helper function to find the square root
- * @n: The number for which to calculate the square root
- * @guess: The current guess for the square root
- * Return: The natural square root of n, or -1 if no exact square root is found
- */
-int sqrt_helper(int n, int guess)
-{
-	if (guess * guess == n)
-		return (guess);
-	if (guess * guess > n)
-		return (-1);
-	return (sqrt_helper(n, guess + 1));
+		_sqrt_recursion(n - 1);
 }
