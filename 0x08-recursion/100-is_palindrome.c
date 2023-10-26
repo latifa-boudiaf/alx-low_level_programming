bdc22c517b1f7a91bfebe5bb67fc013554593aaa
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-26 11:08:20 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * length - returns the length of a string.
+ * @s: the string to count
+ * Return: length of the string
+ */
+
+int length(char* s)
+{
+	if(s*)
+	{
+		s++;
+		return(length(s) + 1);
+	}
+	else
+		return 0;
+}
+
+/**
+ * palindrome - helper function for is_palindrome
+ * @s: the string
+ * @l: length of string
+ * @i: counter of recursion
+ * Return: 1 if string is a palindrome, 0 if it is not.
+ */
+
+int palindrome(char *s, int l, int i)
+{
+	if( i == l)
+		return 1;
+	else
+	{
+		if(s[i] == s[l])
+			return palindrome(s, l - 1, i + 1);
+		else
+			return 0;
+	}
+}
+
+/**
+ * is_palindrome - checks if the string is a palindrome
+ * @s: the string to check
+ * Return: 1 if string is a palindrome, 0 if it is not.
+ */
+
+int is_palindrome(char *s)
+{
+	return palindrome(s, length(s) - 1, 0);
+}	
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,6 +10,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrt_helper(int n, int guess);
 int is_prime_number(int n);
+int is_palindrome(char *s);
 
 
 #endif
