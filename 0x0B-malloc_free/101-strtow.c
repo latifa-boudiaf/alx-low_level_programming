5ba5bcdd611ef41dbca55397276424ec9f9766f8
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-31 14:41:26 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -51,11 +51,15 @@ char **strtow(char *str)
 {
 	int total_words = 0, b = 0, c = 0, length = 0;
 	char **words, *found_word;
-	if (str == 0 || *str == 0) return (NULL);
+
+	if (str == 0 || *str == 0) 
+		return (NULL);
 	total_words = countWords(str);
-	if (total_words == 0) return (NULL);
+	if (total_words == 0) 
+		return (NULL);
 	words = malloc((total_words + 1) * sizeof(char *));
-	if (words == 0) return (NULL);
+	if (words == 0) 
+		return (NULL);
 	for (; *str != '\0' &&  b < total_words;)
 	{
 		if (*str == ' ')
