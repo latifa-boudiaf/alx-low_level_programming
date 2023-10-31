16e25c5f0204d847b92b290c4039589334e662fa
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-31 15:14:10 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,31 +1,95 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+
 /**
- * strtow - concatenates arguments.
- * @str: String to be splitted.
+ * countWords - function to calculate number of words
+ * @str: string being passed to check for words
  *
- * Return: a pointer to array of String.
+ * Return: countWords of words
  */
+
+int countWords(char *str)
+{
+	int a, total = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		if (*str == ' ')
+			str++;
+		else
+		{
+			for (; str[a] != ' ' && str[a] != '\0'; a++)
+				str++;
+			total++;
+		}
+	}
+	return (total);
+}
+
+/**
+ * free_all - frees the memory
+ * @string: pointer values being passed for freeing
+ * @i: counter
+ */
+
+void free_all(char **string, int i)
+{
+	for (; i > 0;)
+		free(string[--i]);
+	free(string);
+}
+
+
+/**
+ * strtow - function that splits string into words
+ * @str: string being passed
+ * Return: null if string is empty or null or function fails
+*/
+
 char **strtow(char *str)
 {
-	char *array = NULL;
-	unsigned int i = 0, j = 0, k;
+	int total_words = 0, b = 0, c = 0, length = 0;
+	char **words, *found_word;
 
-	if (strncmp(str, "", 1) || str == NULL)
+	if (str == 0 || *str == 0) 
 		return (NULL);
-	array = malloc((i + j + 1) * sizeof(char));
-	if (array == NULL)
+	total_words = countWords(str);
+	if (total_words == 0) 
 		return (NULL);
-	for (k = 0; k < i; k++)
-		array[k] = str[k];
-	i = k;
-	for (k = 0; k < j; k++)
+	words = malloc((total_words + 1) * sizeof(char *));
+	if (words == 0) 
+		return (NULL);
+	for (; *str != '\0' &&  b < total_words;)
 	{
-		array[i] = str[k];
-		i++;
+		if (*str == ' ')
+			str++;
+		else
+		{
+			found_word = str;
+			for (; *str != ' ' && *str != '\0';)
+			{
+				length++;
+				str++;
+			}
+			words[b] = malloc((length + 1) * sizeof(char));
+			if (words[b] == 0)
+			{
+				free_all(words, b);
+				return (NULL);
+			}
+			while (*found_word != ' ' && *found_word != '\0')
+			{
+				words[b][c] = *found_word;
+				found_word++;
+				c++;
+			}
+			words[b][c] = '\0';
+			b++;
+			c = 0;
+			length = 0;
+			str++;
+		}
 	}
-	array[i] = '\0';
-	return (NULL);
+	return (words);
 }
