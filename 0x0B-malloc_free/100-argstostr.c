6c572e049fbe4533b8afe6b2678f7aa428c898fe
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-10-31 13:12:30 -0700

    advanced 1

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include "main.h"
 /**
- *
+ * argstostr - concatenates all the arguments.
+ * @ac: number of arguments.
+ * @av: list of arguments.
+ * Return: the concatenated arguments.
  */
 char *argstostr(int ac, char **av)
 {
