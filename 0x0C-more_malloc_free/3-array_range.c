cc2abce5d077a4f74da2044cb2439955099a2350
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-11-02 12:18:34 -0700

    3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * array_range - Create array of integers filled with numbers from min to max
+ * @min: First number in the array
+ * @max: Largest number in the array
+ *
+ * Return: Pointer to new array, NULL if failed
+ */
+
+int *array_range(int min, int max)
+{
+	int *arr;
+	int arr_size, i, j;
+
+	arr_size = max - min + 1;
+	
+	if(arr_size <= 0)
+		return (NULL);
+
+	arr = malloc(sizeof(int) * arr_size);
+	
+	for (j = 0, i = min; i <= max; j++, i++)
+		arr[j] = i;
+
+	return (arr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+int *array_range(int min, int max);
 
 #endif /* MAIN_H */
