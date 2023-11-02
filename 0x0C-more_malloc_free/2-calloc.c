8399aef98ac9645cc541d364a4b43b10059feaf4
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-11-02 15:06:05 -0700

    2-calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory for an array.
+ * @nmemb: the elements of the array.
+ * @size: the size of the array.
+ * Return: pointer to the allocated memory.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int i, total_size;
+	unsigned char *byte_ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+	
+	total_size = nmemb * size;
+	*byte_ptr = (unsigned char *)ptr;
+	for (i = 0; i < total_size; i++)
+		byte_ptr[i] = 0;
+	return (ptr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,5 +4,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int *array_range(int min, int max);
 void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif /* MAIN_H */
