e23889abd86107ca97d09046e6a86307adcbcb8c
latifa-boudiaf <141144184+latifa-boudiaf@users.noreply.github.com>
2023-11-06 20:50:17 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
-  * main - Entry point
-  * Description: Prints the name of the source file.
-  * Return: Always 0 (Success)
-  */
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     printf("%s\n", __FILE__);
