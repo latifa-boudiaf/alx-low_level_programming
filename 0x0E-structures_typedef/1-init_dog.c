ec53fe418ac31ab61d021b86748ac7426af0d796
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-11-06 13:17:18 -0800

    first commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+  * init_dog - initialize struct dog.
+  * @d: name of struct
+  * @name: name of dog in struct
+  * @age: age of dog in struct
+  * @owner: name of owner of dog in struct
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{	
+		if (d.name)
+			printf("Name: %s\n", d.name);
+		else
+			printf("Name: (nil)\n");
+
+		printf("Age: %f\n", d.age);
+
+		if (d.owner)
+			printf("Owner: %s\n", d.Owner);
+		else
+			printf("Owner: (nil)\n");
+	}
