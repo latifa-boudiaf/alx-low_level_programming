c745dc4b3ee1d2c89b003e66f97e5032f0562cec
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-11-07 11:35:50 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - dog data
  * @name: name of the dog
  * @age: age of the dog
  * @owner: name of the owner of the dog
- * Description: Defines metadata about the dog and type of dog's struct
+ * Description: Defines metadata about the dog
  */
-
 typedef struct dog 
 {
 	char *name;
