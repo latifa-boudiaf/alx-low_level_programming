413cb21334618d67b4a03c7023e8c19db6aebc08
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-11-07 12:47:31 -0800

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,40 +1,48 @@
-#include "dog.h"
 #include <stdlib.h>
+#include <string.h>
+#include "dog.h"
 
 /**
-  * new_dog - creates a new struct of type dog
-  * @name: struct parameter name
-  * @age: struct parameter age
-  * @owner: struct parameter owner
-  * Return: returns pointer to buffer of datatype dog
-  */
-
+ * new_dog - creates a new struct of type dog
+ * @name: struct parameter name
+ * @age: struct parameter age
+ * @owner: struct parameter owner
+ * Return: returns pointer to a new dog_t struct
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int len_name, len_owner, i;
+	int len_name, len_owner;
 	dog_t *doggy;
 
-	len_name = len_owner = 0;
-	while (name[len_name++])
-		;
-	while (owner[len_owner++])
-		;
-	doggy = malloc(sizeof(dog_t));
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	len_name = strlen(name);
+	len_owner = strlen(owner);
+
+	doggy = malloc(sizeof(dog_t);
 	if (doggy == NULL)
 		return (NULL);
 
-	doggy->name = malloc(len_name * sizeof(doggy->name));
-	if (doggy == NULL)
+	doggy->name = malloc((len_name + 1) * sizeof(char));
+	if (doggy->name == NULL)
+	{
+		free(doggy);
 		return (NULL);
-	for (i = 0; i < len_name; i++)
-		doggy->name[i] = name[i];
+	}
+	strcpy(doggy->name, name);
 
 	doggy->age = age;
 
-	doggy->owner = malloc(len_owner * sizeof(doggy->owner));
-	if (doggy == NULL)
+	doggy->owner = malloc((len_owner + 1) * sizeof(char));
+	if (doggy->owner == NULL)
+	{
+		free(doggy->name);
+		free(doggy);
 		return (NULL);
-	for (i = 0; i < len_owner; i++)
-		doggy->owner[i] = owner[i];
+	}
+	strcpy(doggy->owner, owner);
+
 	return (doggy);
 }
+
