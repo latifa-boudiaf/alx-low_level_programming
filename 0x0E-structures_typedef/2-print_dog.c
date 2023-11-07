4afe5da35df43f8cdd6b077fd4d907a0bfe2443d
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-11-07 11:26:17 -0800

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,22 +5,14 @@
 /**
   * print_dog - prints the dog struct
   * @d: pointer to the dog struct
-  */
+  * */
 
 void print_dog(struct dog *d)
 {
 	if (d)
-	{	
-		if (d.name)
-			printf("Name: %s\n", d.name);
-		else
-			printf("Name: (nil)\n");
-
-		printf("Age: %f\n", d.age);
-
-		if (d.owner)
-			printf("Owner: %s\n", d.Owner);
-		else
-			printf("Owner: (nil)\n");
+	{
+		d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil)\n"));
+		printf("Age: %f\n", d->age);
+		d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)\n"));
 	}
 }
