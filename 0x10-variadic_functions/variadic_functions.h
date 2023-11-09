01c4a52d93757b2a76301af7be5ddab86ea06423
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-11-09 10:38:48 -0800

    0-sum_them_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,18 +9,5 @@ void print_all(const char * const format, ...);
 
 int _putchar(char c);
 
-/**
- * struct funckey - a key for picking functions to use for printing
- * in 3-print_all.c
- *
- * @f: function to use for printing
- * @spec: character specifier
- */
-typedef struct funckey
-{
-	void (*f)(va_list);
-	char spec;
-} funckey;
-
 
 #endif
