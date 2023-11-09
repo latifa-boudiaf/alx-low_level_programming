d8fb1e9481daabfd4107d02a17d7a4d93506f125
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-11-09 14:00:21 -0800

    1 commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,95 +1,55 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
- * print_char - prints a char
- * @arg: argument to print
+ * print_all - Prints anything based on the format string.
+ * @format: A list of types of arguments to be printed.
+ * Description: This function prints arguments according to the given format.
+ * Supported format characters: c (char), i (integer), f (float), s (char*).
+ * If the string is NULL, it prints "(nil)".
+ * Any other format character is ignored.
+ * A new line is printed at the end.
  */
-
-void print_char(va_list arg)
+void print_all(const char * const format, ...)
 {
-    printf("%c", va_arg(arg, int));
+	unsigned int i;
+	char *separator;
+	char *str;
+	va_list args;
+	va_start(args, format);
+	i = 0;
+	separator = "";
+
+
+	while (format != NULL && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", separator, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+			case 's':
+				{
+					str = va_arg(args, char *);
+					if (str == NULL)
+						str = "(nil)";
+					printf("%s%s", separator, str);
+					break;
+				}
+			default:
+				break;
+		}
+		separator = ", ";
+		i++;
+	}
+
+	va_end(args);
+	printf("\n");
 }
-
-/**
- * print_int - prints an int
- * @arg: argument to print
- */
-
-void print_int(va_list arg)
-{
-    printf("%d", va_arg(arg, int));
-}
-
-/**
- * print_float - prints a float
- * @arg: argument to print
- */
-
-void print_float(va_list arg)
-{
-    printf("%f", (float)va_arg(arg, double));
-}
-
-/**
- * print_string - prints a string
- * @arg: argument to print
- */
-
-void print_string(va_list arg)
-{
-    char *str = va_arg(arg, char *);
-
-    if (str == NULL)
-        printf("(nil)");
-    else
-        printf("%s", str);
-}
-
-/**
- * print_all - prints various types given a format string for the arguments
- * @format: string containing type information for args
- */
-
-void print_all(const char *const format, ...)
-{
-    va_list args;
-    int i = 0, j = 0;
-    char *sep = "";
-
-    typedef struct {
-        char spec;
-        void (*printer)(va_list);
-    } format_specifier;
-
-    format_specifier specifiers[] = {
-        {'c', print_char},
-        {'i', print_int},
-        {'f', print_float},
-        {'s', print_string},
-    };
-
-    va_start(args, format);
-
-    while (format && format[i])
-    {
-        j = 0;
-        while (j < 4 && format[i] != specifiers[j].spec)
-            j++;
-
-        if (j < 4)
-        {
-            printf("%s", sep);
-            specifiers[j].printer(args);
-            sep = ", ";
-        }
-
-        i++;
-    }
-
-    printf("\n");
-
-    va_end(args);
-}
-
