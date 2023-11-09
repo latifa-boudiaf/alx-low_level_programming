49f572eab795e6281e7896b2f295b184bb6239f3
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-11-09 12:56:48 -0800

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,41 +2,86 @@
 #include <stdio.h>
 
 /**
+ * print_char - prints a char
+ * @arg: argument to print
+ */
+
+void print_char(va_list arg)
+{
+    printf("%c", va_arg(arg, int));
+}
+
+/**
+ * print_int - prints an int
+ * @arg: argument to print
+ */
+
+void print_int(va_list arg)
+{
+    printf("%d", va_arg(arg, int));
+}
+
+/**
+ * print_float - prints a float
+ * @arg: argument to print
+ */
+
+void print_float(va_list arg)
+{
+    printf("%f", (float)va_arg(arg, double));
+}
+
+/**
+ * print_string - prints a string
+ * @arg: argument to print
+ */
+
+void print_string(va_list arg)
+{
+    char *str = va_arg(arg, char *);
+
+    if (str == NULL)
+        printf("(nil)");
+    else
+        printf("%s", str);
+}
+
+/**
  * print_all - prints various types given a format string for the arguments
  * @format: string containing type information for args
  */
 
-void print_all(const char * const format, ...)
+void print_all(const char *const format, ...)
 {
     va_list args;
-    int i = 0;
-    char *str;
+    int i = 0, j = 0;
+    char *sep = "";
+
+    typedef struct {
+        char spec;
+        void (*printer)(va_list);
+    } format_specifier;
+
+    format_specifier specifiers[] = {
+        {'c', print_char},
+        {'i', print_int},
+        {'f', print_float},
+        {'s', print_string},
+    };
 
     va_start(args, format);
 
     while (format && format[i])
     {
-        if (i > 0)
-            printf(", ");
+        j = 0;
+        while (j < 4 && format[i] != specifiers[j].spec)
+            j++;
 
-        switch (format[i])
+        if (j < 4)
         {
-            case 'c':
-                printf("%c", va_arg(args, int));
-                break;
-            case 'i':
-                printf("%d", va_arg(args, int));
-                break;
-            case 'f':
-                printf("%f", va_arg(args, double));
-                break;
-            case 's':
-                str = va_arg(args, char *);
-                if (str == NULL)
-                    printf("(nil)");
-                else
-                    printf("%s", str);
-                break;
+            printf("%s", sep);
+            specifiers[j].printer(args);
+            sep = ", ";
         }
 
         i++;
