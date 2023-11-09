48dd8892a1993c205b9e0a2eb2b296b30c8632b7
latifa-boudiaf <latifa.boudiaf@univ-constantine2.dz>
2023-11-09 10:34:07 -0800

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums all arguments passed
+ *
+ * @n: number of arguments
+ *
+ * Return: sum of arguments
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
+	va_list list;
+	
+	s = 0;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+		s += va_arg(list, int);
+
+	va_end(list);
+	
+	return (sum);
+}
