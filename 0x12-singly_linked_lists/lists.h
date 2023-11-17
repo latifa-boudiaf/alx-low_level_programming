087c62e35409ab38dd7489acd330a6f7d082c8f9
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-17 06:29:44 -0800

    second commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
 #include <stddef.h>
 
 /**
@@ -24,4 +24,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* MAIN_H */
+#endif /* LISTS_H */
