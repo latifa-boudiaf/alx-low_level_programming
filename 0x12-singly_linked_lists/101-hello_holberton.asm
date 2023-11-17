a9b1d6c66385b853d3208918e28f60dbde89f440
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-17 08:48:42 -0800

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,8 @@
-section .data
-    ; Define a null-terminated string to be printed with a newline
-    hello_string db "Hello, Holberton", 0xA ; 0xA is the newline character
-
-section .text
-    ; Declare the main function
-    global main
-    ; Declare the printf function as an external function
-    extern printf
-
+gloabl	main
+	extern	printf
 main:
-    ; Set up the stack frame
-    push rbp
-    ; Set the format string address in the rdi register
-    mov rdi, hello_string
-    ; Call the printf function
-    call printf
-    ; Clean up the stack frame
-    pop rbp
-    ; Return from the main function
-    ret
-
+	mov	edi, format
+	xor	eax, eax
+	call	printf
+	mov	eax, 0
+format: dv `Hello, Holberton\n`,0
