bd8073fbae37e49dfd6c353c3322df642ca6d34e
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-17 05:10:56 -0800

    0 commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,11 +17,11 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)\n");
+		/*	printf("[0] (nil)\n"); */
 		}
 		else
 		{
-			printf("[%u] %s\n", (unsigned int)h->len, ptr->str);
+		/*	printf("[%u] %s\n", (unsigned int)h->len, ptr->str); */
 		}
 		nbr_nodes++;
 		h = h->next;
