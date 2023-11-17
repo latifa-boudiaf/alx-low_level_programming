f063aa73711d20fe91200ac155d937d4fadf22e3
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-17 07:08:13 -0800

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,23 +11,23 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
+	list_t *new, *current;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->str = strdup(str);
-	new_node->len = strlen(str);
+	new->len = strlen(str);
 	new->next = NULL;
 	if (*head == NULL)
 		*head = new;
 	else
 	{
-		list_t *current = *head;
+		current = *head;
 		while (current->next != NULL)
 			current = current->next;
-		current->next = new_node;
+		current->next = new;
 	}
 
 	return (*head);
