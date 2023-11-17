6d96a49784f99c705bfa3c0c52bbcd2dbbe8aa94
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-17 07:03:17 -0800

    something

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: Pointer to a pointer to the head of the list.
+ * @str: String to be duplicated and set as the new node's str.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
