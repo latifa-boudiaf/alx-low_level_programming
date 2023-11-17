2fa571b503d1d8cca508a76aec918099b3f593dc
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-17 06:25:22 -0800

    second commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,16 +3,15 @@
 #include <stddef.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 
-typedef struct list_s 
-{
+typedef struct list_s {
     char *str;
     unsigned int len;
     struct list_s *next;
