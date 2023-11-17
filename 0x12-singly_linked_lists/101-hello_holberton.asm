3808d3d5b106a51e5683a4d26c80e8b39fae0119
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-17 07:18:37 -0800

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,22 @@
 section .data
+    ; Define a null-terminated string to be printed
     hello_string db "Hello, Holberton", 0
 
 section .text
+    ; Declare the main function
     global main
+    ; Declare the printf function as an external function
     extern printf
 
 main:
+    ; Set up the stack frame
     push rbp
+    ; Set the format string address in the rdi register
     mov rdi, hello_string
+    ; Call the printf function
     call printf
+    ; Clean up the stack frame
     pop rbp
+    ; Return from the main function
     ret
 
