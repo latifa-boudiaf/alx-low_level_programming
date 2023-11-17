322f4375467f1760e746ee2051fa2dc05472db76
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-17 04:47:50 -0800

    first commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * print_list - prints all the elements of a list_t list.
+ * @h: a pinter to the list.
+ * Return: the number of nodes.
+ */
+
+size_t print_list(const list_t *h)
+{
+	list_t *ptr;
+	size_t nbr_nodes;
+
+	ptr = h;
+	nbr_nodes = 0;
+	while (ptr != NULL)
+	{
+		if (ptr->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%zu] %s\n", ptr, ptr->str);
+		}
+		ptr = ptr->next;
+		nbr_nodes++;
+	}
+
+	return (nbr_nodes);
+}
