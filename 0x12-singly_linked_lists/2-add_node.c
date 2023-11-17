424069d875e52722bca1e404b90002de66ae1f34
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-17 06:33:51 -0800

    second commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,19 +13,11 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 
-	if (str == NULL)
-		return (NULL);
-
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return NULL;
-	}
 	new_node->len = strlen(str);
 	new->next = *head;
 	*head = new;
