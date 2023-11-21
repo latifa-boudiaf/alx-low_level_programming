9b2eddf61ead4bf14a0801368b0c7b8da2850a72
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-21 11:56:40 -0800

    1-listint

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,9 +9,9 @@
   */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
-{      
+{
 	listint_t *newNode, *tmp;
-	
+
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
 		return (NULL);
