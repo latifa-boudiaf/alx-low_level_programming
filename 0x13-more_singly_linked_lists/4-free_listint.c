21b1f5d2e18bc4ea10d00eac2b9b10362a9effea
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-21 11:02:20 -0800

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+  * free_listint - define function
+  * @head: describe argument
+  */
+
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+		return;
+	else
+	{
+		free_listint(head->next);
+		free(head);
+	}	
+}
