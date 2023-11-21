071a244dbd45314f1d5bb47842fc54c27f64d3ed
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-21 13:30:36 -0800

    2nd

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,6 +4,7 @@
 /**
  * pop_listint - deletes the head node of a list.
  * @head: the pointer to the first list element.
+ * Return: 0 if empty and the value of the node otherwise.
  */
 
 int pop_listint(listint_t **head)
