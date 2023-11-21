beba8f48de883a74b1c37fcddf96f77b35173a3d
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-21 10:02:06 -0800

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,9 +1,14 @@
 #include "lists.h"
 
+/**
+  * listint_len - gets the number of nodes in a list
+  * @h: singly linked list to print
+  * Return: number of nodes in the list
+  */
 
 size_t listint_len(const listint_t *h)
 {
-	int i;
+	size_t i;
 	
 	for (i = 0; h; i++)
 		h = h->next;
