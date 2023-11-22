c8122adff8c97592f47f082fa29e0ede688ee1f1
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-22 09:19:15 -0800

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 #include <stdlib.h>
-
 /**
- *
- *
- */
+  * sum_listint - sums all elements of a list
+  * @head: pointer to the head of the list
+  * Return: sum of all elements
+  */
 int sum_listint(listint_t *head)
 {
 	int s;
