f358dc3664685c33b07997d902bfe8c7748c2fc1
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-23 10:30:03 -0800

    commit :))

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,24 +11,24 @@
 void print_binary(unsigned long int n)
 {
 	int i, num = 0;
-	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	unsigned long int mask = 1UL << 63;
 
 	for (i = 0; i < 64; i++)
 	{
 		if (n <  2)
 		{
-			putchar('0' + n);
+			_putchar('0' + n);
 			break;
 		}
 		if (n & mask)
 		{
-			putchar('1');
+			_putchar('1');
 			num = 1;
 		}
 		else
 		{
 			if (num == 1)
-				putchar('0');
+				_putchar('0');
 		}
 		mask >>= 1;
 	}
