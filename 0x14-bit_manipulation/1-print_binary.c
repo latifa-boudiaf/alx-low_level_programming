0ee00c0340c6ab69a2a4b2caf740fd12cceeb042
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-23 10:27:13 -0800

    commit :))

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,16 +4,32 @@
 /**
  * print_binary - prints the binary representation of a number.
  * @n: the number to convert.
+ *
+ * Return: void
  */
 
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-	{
-		printf("0");
-		return;
-	}
+	int i, num = 0;
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
 
-	print_binary(n >> 1);
-	printf("%lu", n & 1);
+	for (i = 0; i < 64; i++)
+	{
+		if (n <  2)
+		{
+			putchar('0' + n);
+			break;
+		}
+		if (n & mask)
+		{
+			putchar('1');
+			num = 1;
+		}
+		else
+		{
+			if (num == 1)
+				putchar('0');
+		}
+		mask >>= 1;
+	}
 }
