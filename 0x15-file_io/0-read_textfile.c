8f4204028ce84aee0b55f880a191f86a667755d1
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-30 14:32:34 -0800

    first commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 
 /**
  * read_textfile - reads letters from a file and writes them.
@@ -14,28 +12,28 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
+	FILE *fd;
 	char *buffer;
 	ssize_t bytes_read, bytes_written;
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	fd = fopen(filename, "r");
+	if (fd == NULL)
 		return (0);
 
 	buffer = (char *)malloc(sizeof(char) * (letters + 1));
 	if (buffer == NULL)
 	{
-		close(fd);
+		fclose(fd);
 		return (0);
 	}
 
 	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
 	{
-		close(fd);
+		fclose(fd);
 		free(buffer);
 		return (0);
 	}
@@ -43,12 +41,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
-		close(fd);
+		fclose(fd);
 		free(buffer);
 		return (0);
 	}
 
-	close(fd);
+	fclose(fd);
 	free(buffer);
 	return (bytes_written);
 }
