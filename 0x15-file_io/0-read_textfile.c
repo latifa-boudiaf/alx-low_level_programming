df3654d8adc1eb8601f7f5b66eefbb3941c2c72b
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-30 14:39:59 -0800

    first commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,8 +13,8 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	FILE *fd;
-	char *buffer;
-	ssize_t bytes_read, bytes_written;
+	int i;
+	size_t letters_written = 0;
 
 	if (filename == NULL)
 		return (0);
@@ -23,30 +23,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == NULL)
 		return (0);
 
-	buffer = (char *)malloc(sizeof(char) * (letters + 1));
-	if (buffer == NULL)
+	while ((i = fgetc(fd)) != EOF && letters_written < letters)
 	{
-		fclose(fd);
-		return (0);
-	}
-
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
-	{
-		fclose(fd);
-		free(buffer);
-		return (0);
-	}
-
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		fclose(fd);
-		free(buffer);
-		return (0);
+		_putchar(c);
+		letters_written++;
 	}
 
 	fclose(fd);
-	free(buffer);
-	return (bytes_written);
+	return (letters_written);
 }
