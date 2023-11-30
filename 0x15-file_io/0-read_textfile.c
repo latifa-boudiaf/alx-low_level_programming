63d6abdc53419e3a0716e6454e0e604a3e80dac5
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-30 15:13:40 -0800

    first commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
  * read_textfile - reads letters from a file and writes them.
@@ -10,25 +12,48 @@
  * Return: the number of letters written or 0 in failure.
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fd;
-	int i;
-	size_t letters_written = 0;
-
+	int fd;
+	char *buffer;
+	ssize_t bytes_read, bytes_written;
+	
 	if (filename == NULL)
 		return (0);
 
-	fd = fopen(filename, "r");
-	if (fd == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
 
-	while ((i = fgetc(fd)) != EOF && letters_written < letters)
+	buffer = (char *)malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
 	{
-		putchar(i);
-		letters_written++;
+		close(fd);
+		return (0);
 	}
 
-	fclose(fd);
-	return (letters_written);
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+
+	close(fd);
+	free(buffer);
+	return (bytes_written);
 }
