71fa6af1560b9ceac5b2b46a46e20efbcdcbde0e
root <latifa.boudiaf@univ-constantine2.dz>
2023-11-30 15:36:12 -0800

    first commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,11 +12,6 @@
  * Return: the number of letters written or 0 in failure.
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
